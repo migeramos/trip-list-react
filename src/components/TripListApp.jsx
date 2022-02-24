825302ef988eec258fbb6521f62c2fc52c09458a
Miguel Angel Ramos <miguelangelramos@Miguels-Mac-mini.local>
2022-02-24 09:48:42 -0600

    Add footer and validation for special characters

--- src/components/TripListApp.jsx
+++ src/components/TripListApp.jsx
@@ -3,6 +3,7 @@ import HeaderComponent from "./HeaderComponent";
 import DataComponent from "./DataComponent";
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 import ItemComponent from "./ItemComponent";
+import FooterComponent from "./FooterComponent";
 
 class TripListApp extends Component{
     render(){
@@ -15,6 +16,7 @@ class TripListApp extends Component{
                         <Route path="/items/:id" component={ItemComponent}/>
                         <Route component={DataComponent}/>
                     </Switch>
+                    <FooterComponent/>
                     </>
                 </Router>
             </div>
