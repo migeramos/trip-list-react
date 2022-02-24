073bb7f5c678ba727cace7b9aa4c941f16cbec43
Miguel Angel Ramos <miguelangelramos@Miguels-Mac-mini.local>
2022-02-23 21:42:26 -0600

    Add heroku service url to requests

--- src/api/ItemsTripService.js
+++ src/api/ItemsTripService.js
@@ -2,20 +2,22 @@ import axios from "axios";
 
 class ItemsTripService {
 
+    host = 'https://trip-list-eevee1.herokuapp.com';
+
     retriveAllItems(){
-        return axios.get('http://localhost:8080/trip-list/items');
+        return axios.get(`${this.host}/trip-list/items`);
     }
 
     retriveItemById(id){
-        return axios.get(`http://localhost:8080/trip-list/items/${id}`);
+        return axios.get(`${this.host}/trip-list/items/${id}`);
     }
 
     saveItem(item){
-        return axios.post('http://localhost:8080/trip-list/items', item);
+        return axios.post(`${this.host}/trip-list/items`, item);
     }
 
     deleteItem(id){
-        return axios.delete(`http://localhost:8080/trip-list/items/${id}`);
+        return axios.delete(`${this.host}/trip-list/items/${id}`);
     }
 }
 
